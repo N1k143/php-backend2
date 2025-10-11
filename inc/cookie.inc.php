4ccdfbafc2d77b65c86a93ddb43c1ac0b7102128
nikitapetrunin296.email@example.com <nikitapetrunin296.gmail.com>
2025-10-11 18:32:17 +0500

    LPR7

---
+++ inc/cookie.inc.php
@@ -0,0 +1,25 @@
+<?php
+$visitCounter = 0;
+
+if(isset($_COOKIE['visitCounter'])){
+    $visitCounter = (int)$_COOKIE['visitCounter'];
+}
+
+$visitCounter++;
+
+$lastVisit = "";
+
+if(isset($_COOKIE['lastVisit'])){
+    $lastVisit = date("d-m-Y H-i-s", (int)$_COOKIE['lastVisit']);
+
+}
+
+setcookie("visitCounter", $visitCounter, time() + 3600 * 24 * 30);
+setcookie("lastVisit", time(), time() + 3600 * 24 * 30);
+
+if (!isset($_COOKIE['lastVisit']) || date('d-m-Y', $_COOKIE['lastVisit']) != date('d-m-Y')) {
+    setcookie('visitCounter', $visitCounter, time() + 3600 * 24);
+    setcookie('lastVisit', time(), time() + 3600 * 24);
+}
+
+?>
